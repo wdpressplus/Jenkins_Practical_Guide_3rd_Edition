dbe884bf1c92b42bd46dbf933aee021b343cff46
technicalpractice <38248880+technicalpractice@users.noreply.github.com>
2018-04-25 21:28:53 +0900

    糞糞糞
    テスト

--- src/test/java/jp/gihyo/jenkinsbook/action/SampleActionTest.java
+++ src/test/java/jp/gihyo/jenkinsbook/action/SampleActionTest.java
@@ -30,7 +30,7 @@ public class SampleActionTest {
 		boolean result = action.checkParameter(request);
 		verify(request);
 
-		assertEquals(true, result);
+		assertEquals(false, result);
 	}
 
 	@Test
